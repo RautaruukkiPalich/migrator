b88b542bdca14b218c3af04d093a498fce8229d9
palich <rautaruukkipalich@gmail.com>
2024-05-10 16:59:53 +0500

    edit error

--- migrator/errors.go
+++ migrator/errors.go
@@ -6,7 +6,7 @@ var (
 	ErrInvalidDriver             = errors.New("not a valid driver")
 	ErrFailedToCreateConnection  = errors.New("failed to create connection db")
 	ErrFailedToConnectDB         = errors.New("failed to connect db")
-	ErrInvalidTablename          = errors.New("table name must be one word without comma")
+	ErrInvalidTablename          = errors.New("invalid tablename")
 	ErrParseRow                  = errors.New("cant parse row")
 	ErrFailedToMarshal           = errors.New("failed to marshal")
 	ErrFailedToSendKafkaMessages = errors.New("failed to send kafka messages")
